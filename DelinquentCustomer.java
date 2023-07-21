ebb4dba8f852138b749b096222c2d195e03de262
tmo725 <113779402+tmo725@users.noreply.github.com>
2023-07-21 11:28:17 +0700

    Add files via upload

---
+++ DelinquentCustomer.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author sarun
+ */
+//make this class a subclass of customer
+public class DelinquentCustomer extends Customer {
+
+    //Complete this class
+    @Override
+    public String createMail() {
+        return "Delinquent Customer";
+    }
+    
+}
---
+++ RegularCustomer.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author sarun
+ */
+//make this class a subclass of customer
+public class RegularCustomer extends Customer {
+
+    @Override
+    public String createMail() {
+        return "Regular Customer";
+    }
+}
