bff0c53cba4b501b68a7d2cd8182dc8884b85b1b
tmo725 <113779402+tmo725@users.noreply.github.com>
2023-07-21 11:35:45 +0700

    Add files via upload

---
+++ MountainCustomer.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author sarun
+ */
+//make this class a subclass of customer
+public class MountainCustomer extends Customer  {
+
+    //Complete this class
+    @Override
+    public String createMail() {
+        return "Mountain Customer";
+    }
+}
